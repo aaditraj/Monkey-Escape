abe7b933b77b822663b04162f1b4743f73450c7c
AdithyaG <55636518+AdiGisthebest@users.noreply.github.com>
2022-01-17 16:04:19 -0800

    Small changes

--- ArcadeGame/src/DrawingSurface.java
+++ ArcadeGame/src/DrawingSurface.java
@@ -15,7 +15,7 @@ public class DrawingSurface extends PApplet {
 //		collider1 = new MobileEnemy(10d, 0d, 150d, 400d, 150d, 20d, 20d, 5d, 0d, 1);
 //		collider2 = new MobileEnemy(10d, 400d, 150d, 0d, 150d, 20d, 20d, -5d, 0d, 1);
 		collider1 = new Collider(new String[5], 10d, 0d, 151d, 41d, 30d, 2d, 0d);
-		collider2 = new Collider(new String[5], 10d, 400d, 151d, 21d, 20d, -2d, 0d);
+		collider2 = new Collider(new String[5], 18d, 408d, 151d, 21d, 20d, -2d, 0d);
 	}
 	
 	
@@ -79,31 +79,31 @@ public class DrawingSurface extends PApplet {
 	
 	public void keyPressed() {
 		if (keyCode == UP) { 
-			collider1.moveBy(0, -10, new Collider[] {collider2}, 1);
+			collider1.moveBy(0, -10, new Collider[] {collider2});
 			
 		} else if (keyCode == DOWN) { 
-			collider1.moveBy(0, 10, new Collider[] {collider2}, 1);
+			collider1.moveBy(0, 10, new Collider[] {collider2});
 
 			
 		} else if (keyCode == RIGHT) {
-			collider1.moveBy(10, 0, new Collider[] {collider2}, 1);
+			collider1.moveBy(10, 0, new Collider[] {collider2});
 
 		} else if (keyCode == LEFT) {
-			collider1.moveBy(-10, 0, new Collider[] {collider2}, 1);
+			collider1.moveBy(-10, 0, new Collider[] {collider2});
 
 		} else if (key == 'w') {
-			collider2.moveBy(0, -10, new Collider[] {collider1}, 0);
+			collider2.moveBy(0, -10, new Collider[] {collider1});
 
 		} else if (key == 'd') {
-			collider2.moveBy(10, 0, new Collider[] {collider1}, 0);
+			collider2.moveBy(10, 0, new Collider[] {collider1});
 
 		}
 		else if (key == 's') {
-				collider2.moveBy(0, 10, new Collider[] {collider1}, 0);
+				collider2.moveBy(0, 10, new Collider[] {collider1});
 		
 		}
 		else if (key == 'a') {
-			collider2.moveBy(-10, 0, new Collider[] {collider1}, 0);
+			collider2.moveBy(-10, 0, new Collider[] {collider1});
 	
 		}
 		//etc.
