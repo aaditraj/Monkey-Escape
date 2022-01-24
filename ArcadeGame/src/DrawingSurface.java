86c47190f91c6ebc4ca4a76f8e9c11ca8b4df78e
AdithyaG <55636518+AdiGisthebest@users.noreply.github.com>
2022-01-23 20:17:39 -0800

    update sideshooter

--- ArcadeGame/src/DrawingSurface.java
+++ ArcadeGame/src/DrawingSurface.java
@@ -8,6 +8,7 @@ import enemies.ShootingEnemy;
 import enemies.SideShooter;
 import obstacles.Barrel;
 import obstacles.Lava;
+import players.Player;
 import players.ShootingPlayer;
 import processing.core.PApplet;
 import startpage.Leaderboard;
@@ -32,21 +33,28 @@ public class DrawingSurface extends PApplet implements Serializable{
 	private ShootingEnemy enemy2;
 	private SideShooter sideShooter;
 	private Boolean tester = false;
-	ArrayList<Collider> bullets = new ArrayList<>();
-	ArrayList<Collider> gamePieces = new ArrayList<Collider>();
-	ArrayList<Collider> playerPieces = new ArrayList<Collider>();
-	int playerSpeed = 5;
+	ArrayList<Collider> bullets;
+	ArrayList<Collider> gamePieces;
+	ArrayList<Collider> playerPieces;
+	int playerSpeed = 10;
+	int time = 0;
 	boolean[] keysPressed;
 	public DrawingSurface() {
-		player = new ShootingPlayer(100,100,100,110d,152d,0,0,3000);
+		setup();
+	}
+	public void setup() {
+		player = new ShootingPlayer(10,100,100,110d,152d,0,10,3000);
 		lava = new Lava(0, 0, 300, 650, 100, 0.1);
-		barrel = new Barrel(10,0,10,50,50,0,0);
+		barrel = new Barrel(10,300,300,500,50,0,0);
 		leaderboard = new Leaderboard();
 		keysPressed = new boolean[4];
 		mobileEnemy = new MobileEnemy(10d, 400d, 150d, 0d, 150d, -5d, 0d,(int)424d/4, (int)464d/4, 1);
-		sideShooter = new SideShooter(100,100,0,100,100,-1);
+		sideShooter = new SideShooter(100,200,70,100,100,1);
+		bullets = new ArrayList<>();
+		gamePieces = new ArrayList<Collider>();
+		playerPieces = new ArrayList<Collider>();
 		gamePieces.add(player);
-		//gamePieces.add(barrel);
+		gamePieces.add(barrel);
 		gamePieces.add(mobileEnemy);
 		gamePieces.add(sideShooter);
 		try {
@@ -55,15 +63,6 @@ public class DrawingSurface extends PApplet implements Serializable{
 		} catch (IOException ioe) {
             ioe.printStackTrace();
         }
-		
-		
-
-		
-
-	}
-	
-	
-	public void setup() {
 	}
 	
 	
@@ -72,7 +71,10 @@ public class DrawingSurface extends PApplet implements Serializable{
 		background(50);
 		
 		leaderboard.draw(this);
-		
+		if(time%sideShooter.bulletFrequency == 0) {
+			bullets.add(sideShooter.shoot());
+			System.out.println("shooting");
+		}
 		//System.out.println(mobileEnemy.getHealth());
 		for (int i = 0; i < bullets.size(); i++) {
 			Collider bullet = bullets.get(i);
@@ -90,7 +92,12 @@ public class DrawingSurface extends PApplet implements Serializable{
 		
 		for (int i = 0; i < gamePieces.size(); i++) {
 			if (gamePieces.get(i).getHealth() <= 0) {
-				gamePieces.remove(i);
+				if(gamePieces.get(i) instanceof Player) {
+					setup();
+				} else {
+					gamePieces.remove(i);
+				}
+				
 			} else {
 				gamePieces.get(i).draw(this);
 				//if (gamePieces.get(i) instanceof MobileEnemy) {
@@ -107,7 +114,7 @@ public class DrawingSurface extends PApplet implements Serializable{
 //			mobileEnemy.changeH ealth(20);
 		}
 		move();
-		
+		time++;
 	}
 	
 	public void promptLeaderboard()
--- ArcadeGame/src/enemies/SideShooter.java
+++ ArcadeGame/src/enemies/SideShooter.java
@@ -7,11 +7,11 @@ public class SideShooter extends Collider {
 	//int bulletFrequency = 0;
 	int direction = 0;
 	int bulletSpeed;
-	private int bulletFrequency;
+	public int bulletFrequency;
 	public SideShooter(int x, int y,int bulletFrequency, int width, int height, int direction) {
 		super(new String[] {"assets/SideShooter/SideShooter.png","assets/SideShooter/SideShooterLeft.png"}, Integer.MAX_VALUE,x,y,width,height, 0,0);
-		//1 stands for right, -1 stands for left
-		if(direction == -1) {
+		//-1 stands for right, 1 stands for left
+		if(direction == 1) {
 			goToNextImage();
 		}
 		this.direction = direction;
@@ -19,7 +19,7 @@ public class SideShooter extends Collider {
 		this.bulletSpeed = 10;
 	}
 	public Bullet shoot() {
-		Bullet mc = new Bullet(getX(),getY(),"SideShooter");
+		Bullet mc = new Bullet(getCenterX(),getCenterY(),"SideShooter");
 		mc.setVelocity(direction * bulletSpeed, 0);
 		return mc;
 	}
