24f4ba1a2ece7340699db78a0524055298f4ca97
AdithyaG <incredibleatha2@gmail.com>
2022-04-05 21:34:46 -0700

    commented

--- ArcadeGame/src/core/AnimationDisplayer.java
+++ ArcadeGame/src/core/AnimationDisplayer.java
@@ -2,26 +2,51 @@ package core;
 
 import processing.core.PApplet;
 import processing.core.PImage;
-
+/**
+ * Allows character animations to be displayed
+ * Iterates thorugh image set, and uses frequency to 
+ * determine how fast to iterate though the image set.
+ * eg. Running animation
+ *
+ */
 public class AnimationDisplayer {
 	String[] images;
 	int frequency = 10;
 	int position;
 	int time = 0;
 	boolean animate = true;
+	/**
+	 * Defines a new Animation displayer, that takes in the images 
+	 * it needs to iterate through
+	 * @param imgs
+	 */
 	public AnimationDisplayer(String[] imgs) {
 		images = imgs;
 		time = 0;
 		position = 0;
 	}
+	/**
+	 * Sets the images of the function to the image array passed in
+	 * @param imgs
+	 */
 	public void setImages(String[] imgs) {
 		images = imgs;
 		time = 0;
 		position = 0;
 	}
+	/**
+	 * Makes the image returned stay the same
+	 * Meaning the animator does not animate the image
+	 */
 	public void setAnimateFalse() {
 		animate = false;
 	}
+	/**
+	 * Returns the image that should be displayed, when this image changes, it looks like 
+	 * the character is animated
+	 * @param surface the screen to display the image on
+	 * @return the image to be displayed by surface
+	 */
 	public PImage getImage(PApplet surface) {
 		if(time%frequency == 0 && animate) {
 			position++;
@@ -32,16 +57,32 @@ public class AnimationDisplayer {
 		time++;
 		return surface.loadImage(images[position]);
 	}
+	/**
+	 * Changes the position, so the image returned is of the given position.
+	 * @param position
+	 */
 	public void goToImage(int position) {
 		this.position = position;
 		System.out.println(this.position);
 	}
+	/**
+	 * Sets the frequency/speed of the animation to the passed in speed
+	 * @param speed the animation speed
+	 */
 	public void setFrequency(int speed) {
 		frequency = speed;
 	}
+	/**
+	 * 
+	 * @return The speed of animation
+	 */
 	public int getFrequency() {
 		return frequency;
 	}
+	/**
+	 * 
+	 * @return the position of the image being returned
+	 */
 	public int getPosition() {
 		return position;
 	}
--- ArcadeGame/src/core/Bullet.java
+++ ArcadeGame/src/core/Bullet.java
@@ -9,20 +9,47 @@ public class Bullet extends Collider
 	public static final String bulletLocation = "assets/bullet.png";
 	public String owner = "";
 	int damage = 1;
+	/**
+	 * The constructor that constructs a new bullet
+	 * @param x The x coordinate of the bullet
+	 * @param y The y coordinate of the bullet
+	 * @param owner The thing that shot the bullet
+	 * @param image The image to display for the bullet
+	 */
 	public Bullet(double x, double y, String owner, String image) {
 		super(new String[] {image}, 1, x, y, 5.0, 5.0, 0, 0);
 		this.owner = owner;
 		this.setMobile(true);
 	}
+	/**
+	 * The second bullet constructor
+	 * @param x The X coordinate of the bullet
+	 * @param y The Y coordinate of the bullet
+	 * @param owner The owner of the bullet
+	 * @param image The image to display for the bullet
+	 * @param width The width of the bullet
+	 * @param height The height of the bullet
+	 * @param damage The damage the bulet causes upon impact
+	 */
 	public Bullet(double x, double y, String owner, String image,double width,double height,int damage) {
 		super(new String[] {image}, 1, x, y, width, height, 0, 0);
 		this.owner = owner;
 		this.damage = damage;
 		this.setMobile(true);
 	}
+	/**
+	 * A getter method to return the bullets owner
+	 * @return The bullets owner
+	 */
 	public String getOwner() {
 		return owner;
 	}
+	/**
+	 * A method that moves the bullet, and makes sure it is not colliding wwith any other objects.
+	 * @param x the amount to move the bullet in the x-axis
+	 * @param y the amount to move the bullet in the y-axis
+	 * @param colliders The colliders that it should not go through
+	 */
 	public void moveBy(double x, double y, ArrayList<Collider> colliders) {
 		boolean[] directions = intersects(colliders);
 		
@@ -33,6 +60,10 @@ public class Bullet extends Collider
 			this.x += x;
 		}
 	}
+	/**
+	 * Make the bullet subtract the correct amount of health from the player
+	 * @param collider
+	 */
 	public double collide(Collider collider) {
 		if (owner == "player") {
 			if (collider instanceof MobileEnemy) {
