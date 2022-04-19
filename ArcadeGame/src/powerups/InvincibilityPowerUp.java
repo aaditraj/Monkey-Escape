f26b888948f3b4df117548e8ab638b71848f931e
Julian NTH <jngthowhing627@gmail.com>
2022-04-19 10:40:40 -0700

    asd

--- ArcadeGame/src/powerups/InvincibilityPowerUp.java
+++ ArcadeGame/src/powerups/InvincibilityPowerUp.java
@@ -1,5 +1,11 @@
 package powerups;
 
-public class InvincibilityPowerUp {
+public class InvincibilityPowerUp extends PowerUp {
+
+	public InvincibilityPowerUp(String[] images, double health, double x, double y, double width, double height,
+			double vx, double vy) {
+		super(images, health, x, y, width, height, vx, vy);
+		// TODO Auto-generated constructor stub
+	}
 
 }
