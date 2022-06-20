c6b5388fc2c2519634c38f5f4a17e8d1135e3fd8
Julian NTH <jngthowhing627@gmail.com>
2022-06-19 19:06:39 -0700

    circle

--- ArcadeGame/src/powerups/InvincibilityPowerUp.java
+++ ArcadeGame/src/powerups/InvincibilityPowerUp.java
@@ -13,7 +13,7 @@ public class InvincibilityPowerUp extends PowerUp {
 	public static String[] powerupImages = new String[] {"assets/Powerups/Shield1.png","assets/Powerups/Shield2.png","assets/Powerups/Shield3.png"};
 	public static final String[] playerAnimation = null;
 	private double shieldRad;
-	private PGraphics pGraphics;
+	private PGraphics pg;
 	public InvincibilityPowerUp(ArrayList<Collider> mobilePieces, ArrayList<Collider> bullets, double x, double y, double width, double height) {
 		super(powerupImages, null, mobilePieces, bullets, x, y, width, height, INVINCIBILITY_PERIOD);
 		shieldRad = 150;
@@ -24,17 +24,30 @@ public class InvincibilityPowerUp extends PowerUp {
 	@Override
 	public void drawPowerupEffects(PApplet drawer, Point2D.Double playerLoc) {
 		drawer.push();
-		if(pGraphics == null) {
-			pGraphics = drawer.createGraphics((int)shieldRad/4,(int)shieldRad/4);
-		}
+		if(pg == null) { pg = drawer.createGraphics((int)shieldRad/4,(int)shieldRad/4); }
 		drawer.fill(140, 200, 210, 35);
-		pGraphics.beginDraw();
-		pGraphics.circle((float) shieldRad/8, (float) shieldRad/8,(float) shieldRad/16);
-		pGraphics.endDraw();
-		pGraphics.noSmooth();
-		pGraphics.fill(3, 227, 252, 75);
-		drawer.image(pGraphics,(float) (playerLoc.getX()-shieldRad/2),(float) (playerLoc.getY() - shieldRad/2),(float)shieldRad,(float)shieldRad);
-		drawer.rect(0, 0, drawer.width, drawer.height);
+
+		pg.noSmooth();
+		pg.beginDraw();
+		pg.background(51);
+		pg.noStroke();
+		pg.fill(255);
+		pg.ellipse((int)shieldRad/8, (int)shieldRad/8, (int)shieldRad/4, (int)shieldRad/4);
+		pg.endDraw();
+		pg.fill(3, 227, 252, 75);
+
+		drawer.image(pg, (float) (playerLoc.getX()-shieldRad/2),(float) (playerLoc.getY() - shieldRad/2), (int)shieldRad, (int)shieldRad);
+//		if(pGraphics == null) {
+//			pGraphics = drawer.createGraphics((int)shieldRad/4,(int)shieldRad/4);
+//		}
+//		drawer.fill(140, 200, 210, 35);
+//		pGraphics.beginDraw();
+//		pGraphics.circle((float) shieldRad/8, (float) shieldRad/8,(float) shieldRad/16);
+//		pGraphics.endDraw();
+//		pGraphics.noSmooth();
+//		pGraphics.fill(3, 227, 252, 75);
+//		drawer.image(pGraphics,(float) (playerLoc.getX()-shieldRad/2),(float) (playerLoc.getY() - shieldRad/2),(float)shieldRad,(float)shieldRad);
+//		drawer.rect(0, 0, drawer.width, drawer.height);
 		
 		
 		//drawer.circle((float) playerLoc.getX(), (float) playerLoc.getY(), (float) shieldRad);
